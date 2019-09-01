b0a23f51ce449928dc3a74cc1034beea8edc800c
Natalalia <n.olmo.megias@gmail.com>
2019-09-01 22:29:34 +0100

    controllers up and running

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const slackRouter = require("./routes/slack");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/slack", slackRouter);
 
