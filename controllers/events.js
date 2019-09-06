3c77247405778e88bdf092296f6554f44b4da863
Fernando <fernando.calvo.sanz@gmail.com>
2019-09-06 06:52:05 +0100

    Clean events controller and create messages model

--- controllers/events.js
+++ controllers/events.js
@@ -1,7 +1,5 @@
-const axios = require("axios");
 const { createMoodMessage } = require("../models/moodMessages");
-const botToken = process.env.botToken;
-const slackToken = process.env.slackToken;
+const { sendMessage, deleteMessage } = require("../models/messages");
 
 const eventsController = async (req, res) => {
     const { body } = req;
@@ -9,47 +7,17 @@ const eventsController = async (req, res) => {
         return res.send(req.body.challenge);
     }
     if (body.type === "event_callback" && body.event.channel_type === "im" && !body.event.subtype) {
+        const { channel, ts: tsReceived } = body.event
         await createMoodMessage(body.event.text);
-        axios.post(
-            "https://slack.com/api/chat.postMessage",
-            {
-                text: "Thanks for sharing your feedback, we'll take care of it!",
-                channel: body.event.channel
-            },
-            {
-                headers: {
-                    'Content-type': 'application/json',
-                    'Authorization': 'Bearer ' + botToken
-                }
-            }
-        )
-        .then(({ data }) => {
-            setTimeout(() => {
-                Promise.all([
-                    deleteMessage(data.channel, data.message.ts),
-                    deleteMessage(body.event.channel, body.event.ts)
-                ])
-            }, 2000);
-        })
+        const { message: messageSent } = await sendMessage(channel, "Thanks for sharing your feedback, we'll take care of it!");
+        setTimeout(() => {
+            Promise.all([
+                deleteMessage(channel, messageSent.ts),
+                deleteMessage(channel, tsReceived)
+            ])
+        }, 2000)
         return res.send();
     }
-}
-
-const deleteMessage = (channel, ts) => {
-    return axios.post(
-        "https://slack.com/api/chat.delete",
-        {
-            channel,
-            ts,
-            as_user: true
-        },
-        {
-            headers: {
-                'Content-type': 'application/json; charset=utf-8',
-                'Authorization': 'Bearer ' + slackToken
-            }
-        }
-    )
 }
 
 module.exports = eventsController;
